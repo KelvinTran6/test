06deb137744781f4ca37860cb35062beb747d28c
Kelvin <kelvintran4711@gmail.com>
2020-07-14 12:39:37 -0600

    trying to fix page routing

--- server.js
+++ server.js
@@ -8,17 +8,24 @@ const PORT = process.env.PORT || 3001;
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-// Serve up static assets (usually on heroku)
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static("client/build"));
-}
-
 
 // Add routes, both API and view
 require('./routes/api-routes')(app);
 
 // Connect to the Mongo DB
 mongoose.connect(process.env.MONGODB_URI || "mongodb://kelvin:kelvintran1@ds259787.mlab.com:59787/heroku_c7rz15dv");
+
+
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+  const path = require("path");
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
+
+
 
 // Start the API server
 app.listen(PORT, function() {
