0a4053665ed1bcc8c2f4ae1604045cb1b310095e
Kelvin <kelvintran4711@gmail.com>
2020-07-14 03:33:24 -0600

    fixing routing bug

--- server.js
+++ server.js
@@ -11,7 +11,11 @@ app.use(express.json());
 // Serve up static assets (usually on heroku)
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
+  app.get('*', (request, response) => {
+    response.sendFile(path.join(__dirname, 'client/build', 'index.html'));
+  });
 }
+
 
 // Add routes, both API and view
 require('./routes/api-routes')(app);
