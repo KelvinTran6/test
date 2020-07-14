9518eb53687fdd7fdf1b1ab8da13e200c66b8070
Kelvin <kelvintran4711@gmail.com>
2020-07-14 02:43:50 -0600

    added navbar, no functionality at the moment

--- client/src/App.js
+++ client/src/App.js
@@ -76,7 +76,7 @@
 //               onDelete={this.deleteHandler}
 //             />))
 //         }
-        
+
 //       </div>
 //     );
 //   }
@@ -85,54 +85,60 @@
 // export default App;
 
 import React from 'react';
+import Navbar from './components/Navbar'
 import Row from './components/Row.js'
+import 'bootstrap/dist/css/bootstrap.min.css'
 import * as $ from 'axios';
 
 
 class App extends React.Component {
 
-  constructor(props){
+  constructor(props) {
     super(props)
     this.state = {
       books: [],
     }
   }
 
-  clicked(){
+  clicked() {
 
     const data = {
       title: "12125125arertsd125",
       content: "testing content",
-      url:"https://api.time.com/wp-content/uploads/2014/07/301386_full1.jpg?w=600&quality=85",
+      url: "https://api.time.com/wp-content/uploads/2014/07/301386_full1.jpg?w=600&quality=85",
       description: "testing description"
     }
 
 
     console.log("hello")
     $.post('/api/add', data)
-    .then (res => console.log(res.data));
+      .then(res => console.log(res.data));
   }
 
-  componentDidMount(){
+  componentDidMount() {
     $.get('/api/books')
-    .then(res => {
-      const books = res.data
-      let allBooks = []
+      .then(res => {
+        const books = res.data
+        let allBooks = []
 
-      books.forEach(book => {
-        console.log(book)
-        const current = <Row info={book} />
-        allBooks.push(current)
+        books.forEach(book => {
+          console.log(book)
+          const current = <Row info={book} />
+          allBooks.push(current)
+        })
+        this.setState({ books: allBooks })
       })
-      this.setState({books:allBooks})
-    })
   }
 
-  
-  render(){
+
+  render() {
     return (
-      <div className="page">
-        {this.state.books}
+      <div>
+        <Navbar />
+        <div className="page">
+          {this.state.books}
+        </div>
+
       </div>
     );
 
