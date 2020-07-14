2a1ac43c7717f5df7b86f084f35157937d42908b
Kelvin <kelvintran4711@gmail.com>
2020-07-14 05:52:43 -0600

    edited server file

--- server.js
+++ server.js
@@ -11,9 +11,6 @@ app.use(express.json());
 // Serve up static assets (usually on heroku)
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
-  app.get('*', (request, response) => {
-    response.sendFile(path.join(__dirname, 'client','build', 'index.html'));
-  });
 }
 
 
